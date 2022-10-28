651842ef3cd3b6154128ee56ac028ed37571bbbd
Alex Strugacz <alexstrugacz@icloud.com>
2022-10-27 23:59:29 -0500

    expense bar

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -2,10 +2,20 @@ import React from "react";
 import "./ChartBar.css";
 //Expense Statistics Bar
 const ChartBar = (props) => {
+  let barFillHeight = "0%";
+
+  if (props.max > 0) {
+    //calculate bar height for css
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+  }
+
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
-        <div className="chart-bar__fill"></div>
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
       </div>
       <div className="chart-bar__label">{props.label}</div>
     </div>

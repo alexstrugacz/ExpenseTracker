7983df646487d10bb6034ef10170c629e1169d18
Alex Strugacz <alexstrugacz@icloud.com>
2022-10-28 00:38:00 -0500

    dynamic expense graph tracker

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,13 +3,16 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  //use "..." spread operator to switch dataPoints array into standalone argument, returns 12 arguments (12 months)
+  const totalMaximum = Math.max(...dataPointValues);
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalMaximum}
           label={dataPoint.label}
         />
       ))}
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import React from "react";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -24,6 +25,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           showYearHandler={addFilterByYear}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </li>

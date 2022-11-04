f4757a5e746314b5ef389e965f46bfe1ff84f4da
Alex Strugacz <alexstrugacz@icloud.com>
2022-11-03 21:21:41 -0500

    live firebase build + read me

---
+++ readme.md
@@ -0,0 +1,4 @@
+This is a basic expense tracker build with ReactJS framework.
+
+Please check out the live version at https://expensetracker-8bb26.web.app/
+to see the Expense Tracker in all of its glory.
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import firebase from "firebase/app";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+firebase.initializeApp({
+  apiKey: "AIzaSyBwmsNdPj-Kdszor0aRunAduqy_ND-UDl4",
+  authDomain: "expensetracker-8bb26.firebaseapp.com",
+  projectId: "expensetracker-8bb26",
+  storageBucket: "expensetracker-8bb26.appspot.com",
+  messagingSenderId: "319086887686",
+  appId: "1:319086887686:web:6a9163de037a1427e71487",
+  measurementId: "G-FBBT9QVMPT",
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);

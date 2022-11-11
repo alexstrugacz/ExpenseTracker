d381a409cdcb34d67ebd3d8872267e7534ed3537
Alex Strugacz <alexstrugacz@icloud.com>
2022-11-11 11:59:32 -0600

    comments

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,6 +2,8 @@ import React from "react";
 import ChartBar from "./ChartBar";
 import "./Chart.css";
 
+//Sleep
+//Dynamic chart corresponding to user input expense information
 const Chart = (props) => {
   const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
   //use "..." spread operator to switch dataPoints array into standalone argument, returns 12 arguments (12 months)
